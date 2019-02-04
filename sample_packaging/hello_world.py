d0c90cdec6862d2944ef70736f68e190fa1698ed
Kartik Iyyer <kiyyer@juniper.net>
2019-02-03 20:09:45 -0800

    Added 1 function which can be used without importing it in init. Added comments in python file to understand the functionality better

--- sample_packaging/hello_world.py
+++ sample_packaging/hello_world.py
@@ -1,2 +1,34 @@
 def hello():
-    print 'Hello World!!!'
+    print 'Hello World !!!'
+
+
+def hello_wo_importing():
+    print 'Hello World without importing!!!'
+
+'''
+kiyyer-mbp:~ kiyyer$ python
+Python 2.7.14 (default, Sep 25 2017, 09:53:17) 
+[GCC 4.2.1 Compatible Apple LLVM 8.0.0 (clang-800.0.42.1)] on darwin
+Type "help", "copyright", "credits" or "license" for more information.
+>>> from sample_packaging import hello_world
+>>> hello_world.hello()
+Hello World !!!
+>>> hello_world.hello_wo_importing()
+Hello World without importing!!!
+>>> exit()
+kiyyer-mbp:~ kiyyer$ python
+Python 2.7.14 (default, Sep 25 2017, 09:53:17) 
+[GCC 4.2.1 Compatible Apple LLVM 8.0.0 (clang-800.0.42.1)] on darwin
+Type "help", "copyright", "credits" or "license" for more information.
+>>> import sample_packaging
+>>> sample_packaging.hello_world.hello()
+Hello World !!!
+>>> sample_packaging.hello_world.hello_wo_importing()
+Hello World without importing!!!
+>>> sample_packaging.hello()
+Hello World !!!
+>>> sample_packaging.hello_wo_importing()
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+AttributeError: 'module' object has no attribute 'hello_wo_importing'
+'''
